aa4899c5fd213baf38b6cf7ee1e2fbd146e50e25
reZerOR <gkraihan14k@gmail.com>
2024-09-23 12:12:01 +0600

    Add Redux and React Router setup and shandcn ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { router } from "./routes";
+import { RouterProvider } from "react-router-dom";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
